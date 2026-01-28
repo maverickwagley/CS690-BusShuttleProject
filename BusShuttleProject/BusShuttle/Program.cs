dcd26f5e03089515312c9c66a545fcfaf9a7fe1a
Maverick Wagley <103015775+maverickwagley@users.noreply.github.com>
2026-01-28 02:50:24 +0000

    feat: added basic driver mode

--- BusShuttleProject/BusShuttle/Program.cs
+++ BusShuttleProject/BusShuttle/Program.cs
@@ -1,9 +1,27 @@
 ﻿namespace BusShuttle;
-
+using System.IO;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please Select Mode (driver or manager)");
+        string mode = Console.ReadLine();
+
+        if (mode == "driver"){
+            string command;
+            do{
+                //Get Data
+                Console.WriteLine("Enter stop name");
+                string stopName = Console.ReadLine();
+                Console.WriteLine("Enter number of passangers boarding");
+                int numBoarded = int.Parse(Console.ReadLine());
+                //Add Data to File
+                File.AppendAllText("passenger-data.txt",stopName + ":" + numBoarded+ Environment.NewLine);
+                //Continue or End
+                Console.WriteLine("Enter command (continue or end)");
+                command = Console.ReadLine();
+            } while (command != "end");
+            
+        }
     }
 }
