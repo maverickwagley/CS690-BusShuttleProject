1622068959c135c53e7f5d3518a0477f06a79c7c
Maverick Wagley <103015775+maverickwagley@users.noreply.github.com>
2026-01-29 02:38:06 +0000

    chore: refactor for filesaver and consoleui

---
+++ BusShuttleProject/BusShuttle/FileSaver.cs
@@ -0,0 +1,17 @@
+
+using System.IO;
+
+namespace BusShuttle;
+public class FileSaver 
+{ 
+    string fileName; 
+    public FileSaver(string fileName)
+    { 
+        this.fileName = fileName; 
+        File.Create(this.fileName).Close(); 
+    } 
+    public void AppendLine(string line) 
+    { 
+        File.AppendAllText(this.fileName, line + Environment.NewLine);
+    } 
+} 
--- BusShuttleProject/BusShuttle/Program.cs
+++ BusShuttleProject/BusShuttle/Program.cs
@@ -1,27 +1,10 @@
 ﻿namespace BusShuttle;
-using System.IO;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please Select Mode (driver or manager)");
-        string mode = Console.ReadLine();
-
-        if (mode == "driver"){
-            string command;
-            do{
-                //Get Data
-                Console.WriteLine("Enter stop name");
-                string stopName = Console.ReadLine();
-                Console.WriteLine("Enter number of passangers boarding");
-                int numBoarded = int.Parse(Console.ReadLine());
-                //Add Data to File
-                File.AppendAllText("passenger-data.txt",stopName + ":" + numBoarded+ Environment.NewLine);
-                //Continue or End
-                Console.WriteLine("Enter command (continue or end)");
-                command = Console.ReadLine();
-            } while (command != "end");
-            
-        }
+        ConsoleUI theUI = new ConsoleUI(); 
+        theUI.Show(); 
     }
 }
